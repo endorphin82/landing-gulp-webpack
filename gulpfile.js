e8321d2f27ee49fe1c2ad642217256327d26415b
endorphin82 <endorphin82@gmail.com>
2018-05-08 21:21:09 +0300

    boostrap err

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
   rename = require('gulp-rename'),
   uglify = require('gulp-uglify'),
   webpack = require('webpack'),
-  webpackStream = require('webpack-stream');
+  webpackStream = require('webpack-stream'),
+  sass = require('gulp-sass');
 
 gulp.task('scripts', function () {
   return gulp.src('./src/js/app.js')
@@ -30,4 +31,12 @@ gulp.task('scripts', function () {
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./public/'));
-});
+})
+gulp.task('css', function(){
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'))
+})
+gulp.task('watch', function(){
+  gulp.watch('src/**/*.scss', gulp.series('css'))
+})
